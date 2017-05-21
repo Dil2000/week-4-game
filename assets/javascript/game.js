52a34b77d0b699d5fb0542cdfd2fa882cfe5673d
Dil2000 <dnks2000@gmail.com>
2017-05-21 14:15:48 -0400

    sunday updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,76 +2,105 @@
     $(document).ready(function() {
 
 
-// Initial Variables    
+//  Initial Variables    
       var comPick = "";
-      var greenB = "";
-      var redB = "";
-      var blueB = "";
-      var yellowB = "";
-      var total = 0;    
+      var total = "";  
+      var images =["img1","img2","img3","img4"] ;
+      var jewelValues = [];
+      var loses = 0;
+      var wins  = 0;
 
 
-//Generate randoms
+//  import crystal images and generate values for the crystals
+
       CreateRandom = function(){
-        total = 0;
-        console.log("inside random");
-        comPick = Math.floor(Math.random() * 102) + 19;
-        greenB = Math.floor(Math.random() * 12) * 1;
-        blueB = Math.floor(Math.random() * 12) * 1;
-        yellowB = Math.floor(Math.random() * 12) * 1;
-        redB = Math.floor(Math.random() * 12) * 1;
-        $("#computer-pick").html(comPick);
-        console.log("total : " + total);
-        $("#total-value").html(total);
-        console.log("Green : " + greenB + " / Red : " + redB+ " / Blue : " + blueB + " / Yellow : " + yellowB);
-      }
 
+        //Clean();
+
+        total = 0;
+        console.log("back in random");
+
+        $("#total-value").html(total);
+
+        comPick = Math.floor(Math.random() * 100)+ 19;
+        $("#computer-pick").text(comPick);
+
+        for (var i = 0 ; i < images.length ; i++){
+          jewelValues[i] = 0
+          jewelValues[i] = Math.floor(Math.random() * 10) + 1;
+          //Math.floor(Math.random()*(max - min+1) + min);
+         
+          var imageCrystal = $("<img>");
+          imageCrystal.addClass("crystal-image");
+          imageCrystal.attr("src", "assets/images/"+images[i]+".png");
+          imageCrystal.attr("data-crystalvalue",  jewelValues[i]);
+          imageCrystal.css({width : "100px" , height : "100px"});
+          $("#crystals").prepend(imageCrystal);
+          console.log("end of random");
+        }
+      
+      };
+
+// Callin the random function 
       CreateRandom();
 
-// Compare total value
+
+
+//  Compare total value
       function gameOver(){
-        console.log(total);
-        console.log("Total : " + total + " comPick : " + comPick);
         if (total > comPick){
-          alert("You lose");          
+          alert("You lose"); 
+          loses++;
+          $('#loses').text("No of Loses : " + loses);
           CreateRandom();
         }
         else if (total === comPick){
           alert("You Win");
+          wins++;
+          $('#wins').text("No of Wins : " + wins);
           CreateRandom();
         }
       };
 
-// Calculate  total value
-      function Calculate(value){
-        total = total + value;
+
+
+//  Click on jewells event 
+      $(".crystal-image").on("click", function() {
+        console.log("clicked");
+        var crystalValue = ($(this).attr("data-crystalvalue"));
+        console.log("value clicked " + crystalValue);
+        crystalValue = parseInt(crystalValue);
+
+        // New total value
+        total += crystalValue;
         $("#total-value").html(total);
-        console.log(total);
+
+        // Check whether it's equal or greater than computer pick
         gameOver();
-      }
 
-// Button click events
-      $("#btnG").click(function(){
-        Calculate(greenB);
       });
 
-      $("#btnR").click(function(){
-        Calculate(redB);
+
+
+//  Click Play Again
+      $("#again").on("click",function(){
+        total = 0;
+        jewelValues = [0,0,0,0];
+
+        for (var i = 0 ; i < images.length ; i++){
+          $(".crystal-image").remove();
+          $("#crystals").append();
+        }
+        CreateRandom();
       });
 
-      $("#btnB").click(function(){
-        Calculate(blueB);
-      });
-
-      $("#btnY").click(function(){
-        Calculate(yellowB);
-      });
-
-// Find Random Jewels and update the html
 
 
-// create div tags under append the text
-// create divs  alert("Your new score is: " + counter);
 
+//  Create a background image for total marks
+      //$("#computer-pick").css('backgroundImage','url(assets/images/gems.png)');
+      $(".panel-heading").css('backgroundColor','#999999');
+      $(".panel-heading").css('color','#ffffff');
+
+      
 });
-     
